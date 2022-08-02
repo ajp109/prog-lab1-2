ac4ddc6dea17813bc33d1c64b7a04142a3860994
ajp109 <57534418+ajp109@users.noreply.github.com>
2022-08-02 16:56:43 +0100

    Add sleep to main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
     int var = 30;
     
     printf("var: %d\n", var);
-
-    // Do nothing, forever...
-    while (true);
+    
+    // Do nothing, forever, when the program is complete
+    while (true) sleep();
 }
