9ea8c8de9e47ed4b0cb37e8484aa533c3b045f12
ajp109 <andrew.pomfret@york.ac.uk>
2022-08-02 13:41:12 +0000

    Add correct code to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "mbed.h"
 
-int main() {
-    while (true) {
-        sleep();
-    }
+int main()
+{
+    int var = 100;
+    
+    printf("var: %d\n", var);
+
+    // Do nothing, forever...
+    while (true);
 }
